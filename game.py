3c1be1afcb7d822497750f6e4a663363219cc351
Luis González <luisgonzalezm331@gmail.com>
2021-07-21 10:00:23 -0500

    implementation function process() and new instructions in main()

--- game.py
+++ game.py
@@ -1,35 +1,40 @@
 import os
 import random
 
-udscr=[]
-
-def process(input_word):
-    if input_word in udscr:
-        pass
+wo_un=[]
 
 
+def process(input_word,word_is):    
+    global wo_un
+    for i,char in enumerate(word_is):
+        if input_word == char:
+            wo_un.insert(i,input_word)
+            wo_un.pop()
 
-def underscore():    
-    udscr=["_" for x in range(len(word()))]
-    for x in udscr:
-        print(x, end=" ")
+
+def underscore(word_is):
+    global wo_un
+    wo_un = ["_" for x in range(1,len(word_is))]
 
 
 def word():
     a = random.randint(0,171)
     with open("./data.txt", "r", encoding="utf-8") as words:
         return words.readlines()[a]
-    
 
 
 def main():
-    os.system("cls")
-    print("¡Adivina la palabra!")
-    underscore()    
-    input_word = input("\nIngresa una letra: ")
-    process(input_word)
-    
+    word_is = word()
+    input_word=""
+    underscore(word_is) 
 
+    while input_word != word_is or "".join(wo_un) != word_is:
+        os.system("cls")
+        print("¡Adivina la palabra!\n")
+        print(" ".join(wo_un))        
+        input_word = input("\nIngresa una letra: ")
+        process(input_word,word_is)
+   
 
 if __name__ == "__main__":
     main()
