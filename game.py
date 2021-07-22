04754f7a401edb102513425b46ab7fa87117717d
Luis González <luisgonzalezm331@gmail.com>
2021-07-22 09:50:56 -0500

    fix logic bugs in while

--- game.py
+++ game.py
@@ -8,19 +8,18 @@ def process(input_word,word_is):
     global wo_un
     for i,char in enumerate(word_is):
         if input_word == char:
-            wo_un.insert(i,input_word)
-            wo_un.pop()
+            wo_un[i] = input_word
 
 
 def underscore(word_is):
     global wo_un
-    wo_un = ["_" for x in range(1,len(word_is))]
+    wo_un = ["_" for x in range(len(word_is))]
 
 
 def word():
     a = random.randint(0,171)
     with open("./data.txt", "r", encoding="utf-8") as words:
-        return words.readlines()[a]
+        return words.readlines()[a].replace("\n","")
 
 
 def main():
@@ -28,13 +27,14 @@ def main():
     input_word=""
     underscore(word_is) 
 
-    while input_word != word_is or "".join(wo_un) != word_is:
+    while "".join(wo_un) != word_is and input_word != word_is:
         os.system("cls")
         print("¡Adivina la palabra!\n")
         print(" ".join(wo_un))        
         input_word = input("\nIngresa una letra: ")
         process(input_word,word_is)
-   
+    
+    print(f"Correcto, la palabra es: {word_is}")
 
 if __name__ == "__main__":
     main()
